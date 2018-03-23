2833bc84451ff4c5d7567fecf377f8b9b95f6774
oldpotter <oldpottertom@icloud.com>
2018-03-23 10:19:26 +0800

    UI

--- client/pages/tab/components/pad.js
+++ client/pages/tab/components/pad.js
@@ -17,22 +17,8 @@ Component({
 			},
 			{
 				idx: 1, item: [
-					'm', '#', 'b'
-				]
-			},
-			{
-				idx: 2, item: [
-					'add', 'sus', 'maj', 'min', '+', '-', '/'
-				]
-			},
-			{
-				idx: 3, item: [
+					'm', '#', 'b', 'add', 'sus', 'maj', 'min', '+', '-', '/',
 					'1', '2', '3', '4', '5', '6', '7', '9', '11', '13'
-				]
-			},
-			{
-				idx: 4, item: [
-					'DEL', 'OK'
 				]
 			},]
 	},
@@ -42,18 +28,19 @@ Component({
 			const position = event.currentTarget.dataset.position
 			const value = this.data.items[position.split('-')[0]].item[position.split('-')[1]]
 			let chord = this.data.chord
-			if (value === 'DEL') {
-				chord = ''
-			} else if (value === 'OK') {
-				this.setData({
-					show: false
-				})
-				this.triggerEvent('onclickok', this.data.chord, {})
-				return
-			} else {
-				chord = chord + value
-			}
+			chord = chord + value
 			this.setData({ chord })
+		},
+
+		ok() {
+			this.setData({
+				show: false
+			})
+			this.triggerEvent('onclickok', this.data.chord, {})
+		},
+
+		del() {
+			this.setData({ chord: '' })
 		},
 
 	}
